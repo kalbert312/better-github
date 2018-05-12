d7b38747d9376a61aecbae853c794e44b8735c3e
kalbert312 <kyle.albert312@gmail.com>
2018-05-12 14:15:09 -0400

    Adjusted styling of utils js files. Wider page.

--- utils/prepare.js
+++ utils/prepare.js
@@ -1,6 +1,6 @@
-const fileSystem = require('fs-extra')
-const path = require('path')
+const fileSystem = require('fs-extra');
+const path = require('path');
 
-fileSystem.emptyDirSync(path.join(__dirname, '../build'))
+fileSystem.emptyDirSync(path.join(__dirname, '../build'));
 
-require('./generate_manifest')
+require('./generate_manifest');
--- utils/start.js
+++ utils/start.js
@@ -1,14 +1,14 @@
-const webpack = require('webpack')
-const config = require('../webpack.config')
-require('./prepare')
+const webpack = require('webpack');
+const config = require('../webpack.config');
+require('./prepare');
 
 webpack(
-  Object.assign(config, {
-    watch: true
-  }),
-  function (err) {
-    if (err) {
-      throw err
-    }
-  }
-)
+	Object.assign(config, {
+		watch: true,
+	}),
+	function (err) {
+		if (err) {
+			throw err;
+		}
+	},
+);
